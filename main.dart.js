153c7431906ce60445029a9efcf72a6e56ce6639
Pedro <xelespedro@gmail.com>
2025-06-20 12:49:31 -0300

    POSTANDO github

--- main.dart.js
+++ main.dart.js
@@ -80618,7 +80618,7 @@ break}m.ah(new A.ajb(m))
 p=4
 l=window.localStorage.getItem("FlutterSecureStorage.token")
 if(l==null){d=A.dT("Usu\xe1rio n\xe3o autenticado")
-throw A.e(d)}k=A.f4("https://63f7-177-129-251-249.ngrok-free.app/empresas/")
+throw A.e(d)}k=A.f4("https://66ee-177-129-251-249.ngrok-free.app/empresas/")
 c=A.awl("POST",k)
 c.r.m(0,"Authorization","Bearer "+l)
 d=c.x
@@ -80735,7 +80735,7 @@ A.Dc.prototype={
 qY(){var s=0,r=A.Q(t.H),q=1,p=[],o=this,n,m,l,k,j,i,h,g
 var $async$qY=A.M(function(a,b){if(a===1){p.push(b)
 s=q}while(true)switch(s){case 0:o.ah(new A.akB(o))
-k=A.f4("https://63f7-177-129-251-249.ngrok-free.app/auth/login")
+k=A.f4("https://66ee-177-129-251-249.ngrok-free.app/auth/login")
 j=t.N
 i=A.ar(["Content-Type","application/json"],j,j)
 s=2
@@ -80802,7 +80802,7 @@ A.akE.prototype={
 UY(a){var s=0,r=A.Q(t.H),q=this,p,o,n,m,l
 var $async$$1=A.M(function(b,c){if(b===1)return A.N(c,r)
 while(true)switch(s){case 0:p=B.C1.gafz(t.v_.a(a))
-o=A.f4("https://63f7-177-129-251-249.ngrok-free.app/auth/google/callback")
+o=A.f4("https://66ee-177-129-251-249.ngrok-free.app/auth/google/callback")
 n=t.N
 m=A.ar(["Content-Type","application/json"],n,n)
 s=2
