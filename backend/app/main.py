c68d14a9d4d890a63c501ccbc1a902f96e945322
Predo-Predo <xelespedro@gmail.com>
2025-06-23 12:17:48 -0300

    ds

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,8 @@ app = FastAPI(title="API de Empresas/Apps/Builds")
 # --- CORS usando regex para permitir todos os subdomínios do ngrok ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://(.*\.ngrok-free\.app|predo-predo\.github\.io)",
+    #allow_origin_regex=r"https://(.*\.ngrok-free\.app|predo-predo\.github\.io)",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
