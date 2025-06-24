7bde20760a53f6dca349d27dcf7dc0015ff7e900
Pedro <xelespedro@gmail.com>
2025-06-24 15:02:42 -0300

    POSTANDO github

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8721475f47f1e1b743e5c5afa408c89a
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6f3bf83676b03952dfa6f858e270e15a",
+"flutter_bootstrap.js": "2d1440efb62b96d71d20178238dd0e06",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
