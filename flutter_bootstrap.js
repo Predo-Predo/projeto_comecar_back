7e05870fdebd7a78cd876b04f41270f686dd8aa2
Pedro <xelespedro@gmail.com>
2025-07-03 14:21:57 -0300

    POSTANDO github

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3021176612"
+    serviceWorkerVersion: "1971054602"
   }
 });
